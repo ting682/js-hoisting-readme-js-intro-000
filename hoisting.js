8ceb1accd80310519716d599394a019809da5cbc
Ting Chung <tchung682@gmail.com>
2020-05-11 19:00:29 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,21 +11,25 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+  
 
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+
+  sayMy();
 }
